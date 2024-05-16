1d0217bb3fc7bb2479a1e86c84e5c7d18eef2873
ahammer0 <axel.schwindenhammer@gmail.com>
2024-05-16 19:54:27 +0200

    hiding smtp credentials in env file

--- submit_contact.php
+++ submit_contact.php
@@ -2,31 +2,32 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+require_once __DIR__ . "/env.php";
 
 //Load Composer's autoloader
-require 'vendor/autoload.php';
+require "vendor/autoload.php";
 
 $mail = new PHPMailer(true);
-$postData=$_POST;
-$postPrenom=htmlspecialchars($postData['prenom']);
-$postNom=htmlspecialchars($postData['nom']);
-$postMessage=htmlspecialchars($postData['message']);
-$postEmail=htmlspecialchars($postData['email']);
+$postData = $_POST;
+$postPrenom = htmlspecialchars($postData["prenom"]);
+$postNom = htmlspecialchars($postData["nom"]);
+$postMessage = htmlspecialchars($postData["message"]);
+$postEmail = htmlspecialchars($postData["email"]);
 
-$subject='[ahammer.fr] Message de:'.$postPrenom.' '.$postNom;
+$subject = "[ahammer.fr] Message de:" . $postPrenom . " " . $postNom;
 
-try{
+try {
   $mail->SMTPDebug = SMTP::DEBUG_OFF;
   $mail->isSMTP();
-  $mail->Host = 'smtp.mail.ovh.net';
+  $mail->Host = SMTP_HOST;
   $mail->SMTPAuth = true;
-  $mail->Username = 'axel@ahammer.fr';
-  $mail->Password = '3N1I1AV950';
+  $mail->Username = SMTP_USER;
+  $mail->Password = SMTP_PASS;
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
   $mail->Port = 465;
 
-  $mail->setFrom('axel@ahammer.fr','Mailer ahammer.fr');
-  $mail->addAddress('axel.schwindenhammer@gmail.com');
+  $mail->setFrom(SMTP_USER, "Mailer ahammer.fr");
+  $mail->addAddress(REDIRECT_EMAIL);
   $mail->addReplyTo($postEmail);
 
   $mail->isHTML(false);
@@ -34,14 +35,11 @@ try{
   $mail->Body = $postMessage;
 
   $mail->send();
-  echo 'message sent';
-}catch (Exception $e){
+  echo "message sent";
+  header("Refresh:2;url=./index.php");
+} catch (Exception $e) {
   echo "message could not be send. error{$mail->ErrorInfo}";
 }
-
-
-
-
 ?>
 
 
